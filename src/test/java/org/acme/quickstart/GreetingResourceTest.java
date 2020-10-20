94dbfc81100a4c4d13cefd29e3131bc7f65c76e2
Carlos Salinas Gancedo <carlos.salinas.gancedo@mendix.com>
2020-10-20 17:35:33 +0200

    Update application configuration

--- src/test/java/org/acme/quickstart/GreetingResourceTest.java
+++ src/test/java/org/acme/quickstart/GreetingResourceTest.java
@@ -15,7 +15,7 @@ public class GreetingResourceTest {
           .when().get("/hello")
           .then()
              .statusCode(200)
-             .body(is("Webhooks work. Great job Carlos!"));
+             .body(is("Webhooks work. Great job Carlos local!"));
     }
 
 }
